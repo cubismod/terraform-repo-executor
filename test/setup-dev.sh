9da66acdc23d6764693a385bfe0de9a55047fc48
Drew Welch <dwelch@redhat.com>
2023-05-10 11:49:25 -0500

    refactor tests

--- test/setup-dev.sh
+++ test/setup-dev.sh
@@ -10,7 +10,6 @@ vault server -dev -dev-root-token-id=root &
 
 vault secrets enable -version=2 -path=terraform kv
 vault kv put -mount=terraform app-sre/prod-network  name=linguinie
-vault kv put -mount=terraform app-sre/stage-network  name=zitti
 vault policy write tf-executor policy.hcl
 vault auth enable approle
 vault write auth/approle/role/tf-executor token_num_uses=999 policies="tf-executor, default"
