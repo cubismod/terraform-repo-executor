05819525ec674a80d2588f394333f131dea2de34
Ryan Wallace <rywallac@redhat.com>
2023-06-08 13:24:59 -0400

    add custom ca option for git

--- pkg/git.go
+++ pkg/git.go
@@ -2,9 +2,25 @@ package pkg
 
 import (
 	"fmt"
+	"os"
 )
 
 func (e *Executor) cloneRepo() error {
+	// optionally trust internal git server cert
+	caPath := os.Getenv("INTERNAL_GIT_CA_PATH")
+
+	if caPath != "" {
+		args := []string{"-c", fmt.Sprintf(
+			"git config http.sslCAInfo %s",
+			caPath,
+		)}
+
+		_, err := executeCommand(e.workdir, "/bin/sh", args)
+		if err != nil {
+			return err
+		}
+	}
+
 	args := []string{"-c", fmt.Sprintf(
 		// clone repo with specified name and checkout specified ref
 		"git clone %s %s && cd %s && git checkout %s",
