22dae3dadb5a2f9fea437708ec8e8a1eeaaf998e
Ryan Wallace <rywallac@redhat.com>
2023-11-21 16:18:05 -0500

    remote integration tests (#15)

--- test/plan_a/main.tf
+++
@@ -1,9 +0,0 @@
-data "vault_generic_secret" "test" {
-  path = "terraform/app-sre/prod-network"
-}
-
-module "tf-executor-plan-a" {
-    source= "./vpc"
-    name = data.vault_generic_secret.test.data["name"]
-    vpc_cidr = "10.128.0.0/16"
-}
--- test/policy.hcl
+++
@@ -1,7 +0,0 @@
-path "terraform/data/*" {
-  capabilities = ["read", "list"]
-}
-
-path "terraform/metadata/*" {
-  capabilities = ["read", "list"]
-}
