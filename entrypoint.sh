83cbf82890cb4cfce330294d6b9e105cd739b1bd
Ryan Wallace <rywallac@redhat.com>
2025-08-21 15:47:03 -0400

    remove unnused env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Handle custom CA if needed
 if [[ $USE_CUSTOM_CA == "true" ]]; then
     update-ca-trust
 fi
 
-# Set default log flush delay for Vector logging (can be overridden)
-export LOG_FLUSH_DELAY_SECONDS=${LOG_FLUSH_DELAY_SECONDS:-2}
-
-# Execute main application
 /usr/bin/terraform-repo-executor
