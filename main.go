65400bd6f3032a63bfd2fa3f015505e4107dd6bf
Ryan Wallace <rywallac@redhat.com>
2025-08-21 15:27:22 -0400

    get rid of the extra goroutine

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
 	"time"
 
 	"github.com/app-sre/terraform-repo-executor/pkg"
@@ -25,7 +23,6 @@ const (
 	GitlabToken    = "GITLAB_TOKEN"
 	GitEmail       = "GIT_EMAIL"
 	TfParallelism  = "TF_PARALLELISM"
-	LogFlushDelay  = "LOG_FLUSH_DELAY_SECONDS"
 )
 
 func main() {
@@ -33,12 +30,6 @@ func main() {
 	sessionID := fmt.Sprintf("session-%d", time.Now().UnixNano())
 
 	log.Printf("Starting terraform-repo-executor [%s]", sessionID)
-
-	// Setup graceful shutdown handling
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
-
-	done := make(chan error, 1)
 
 	cfgPath := getEnvOrDefault(ConfigFile, "/config.yaml")
 	workdir := getEnvOrDefault(WorkDir, "/tmp/tf-repo")
@@ -50,61 +41,33 @@ func main() {
 	gitlabToken := getEnvOrError(GitlabToken)
 	gitEmail := getEnvOrError(GitEmail)
 	tfParallelism := getEnvOrDefault(TfParallelism, "10")
-	logFlushDelay := getEnvOrDefault(LogFlushDelay, "3")
 
 	tfParallelismInt, err := strconv.Atoi(tfParallelism)
 	if err != nil {
 		log.Fatal("Integer value required for `TF_PARALLELISM` environment variable")
 	}
 
-	logFlushDelayInt, err := strconv.Atoi(logFlushDelay)
+	err = pkg.Run(cfgPath,
+		workdir,
+		vaultAddr,
+		roleID,
+		secretID,
+		gitlabLogRepo,
+		gitlabUsername,
+		gitlabToken,
+		gitEmail,
+		tfParallelismInt,
+	)
+
+	// sleep to let vector flush logs
+	time.Sleep(2 * time.Second)
+
 	if err != nil {
-		logFlushDelayInt = 3
+		log.Printf("Error: %v [%s]", err, sessionID)
+		os.Exit(1)
 	}
-
-	// Run main logic in goroutine
-	go func() {
-		err := pkg.Run(cfgPath,
-			workdir,
-			vaultAddr,
-			roleID,
-			secretID,
-			gitlabLogRepo,
-			gitlabUsername,
-			gitlabToken,
-			gitEmail,
-			tfParallelismInt,
-		)
-		done <- err
-	}()
-
-	// Wait for completion or signal
-	select {
-	case err := <-done:
-		if err != nil {
-			log.Printf("Error: %v [%s]", err, sessionID)
-			performGracefulShutdown(sessionID, logFlushDelayInt)
-			os.Exit(1)
-		}
-		log.Printf("Completed successfully [%s]", sessionID)
-	case sig := <-sigChan:
-		log.Printf("Received signal %v, shutting down [%s]", sig, sessionID)
-	}
-
-	performGracefulShutdown(sessionID, logFlushDelayInt)
+	log.Printf("Completed successfully [%s]", sessionID)
 	os.Exit(0)
-}
-
-func performGracefulShutdown(sessionID string, delaySeconds int) {
-	// Add minimal logging noise to help Vector fingerprinting
-	log.Printf("Shutting down [%s] - %s", sessionID, time.Now().Format(time.RFC3339))
-
-	// Brief delay for log collection
-	if delaySeconds > 0 {
-		time.Sleep(time.Duration(delaySeconds) * time.Second)
-	}
-
-	log.Printf("Shutdown complete [%s]", sessionID)
 }
 
 func getEnvOrDefault(key, defaultValue string) string {
